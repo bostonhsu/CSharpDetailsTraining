a5894614b4e17f9dca9f71b160b7afcfc66e3439
Boston Hsu <bostonhsu@outlook.com>
2017-06-22 09:33:25 +0800

    建议1, done

--- Main/Main/Test.aspx.cs
+++ Main/Main/Test.aspx.cs
@@ -1,17 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace Main
 {
-    public partial class Test : System.Web.UI.Page
+    public partial class Test : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void test()
+        {
+            string a = "str9";
+            string b = "str9";
+
+            string s1 = "abc";
+            s1 = "123" + s1 + "456";
         }
     }
 }
